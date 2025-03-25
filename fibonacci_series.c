2710a90c9ba805213bb8560985945bc84a69c97a
Dhuor <149362455+Makur123@users.noreply.github.com>
2025-03-25 23:01:08 +0530

    Add files via upload

---
+++ fibonacci_series.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+static int operations = 0;
+
+void printFib(int n) {
+    if (n < 1) {  
+        operations++; 
+        printf("Invalid Number of terms\n");
+        operations++; 
+        return;
+    }
+
+    int prev1 = 1;
+    int prev2 = 0;
+    operations += 2; 
+
+    printf("%d ", prev2);
+    operations++; 
+
+    if (n == 1) {
+        operations++;
+        return;
+    }
+
+    printf("%d ", prev1);
+    operations++; 
+    for (int i = 3; i <= n; i++) {
+        operations++; 
+
+        int curr = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = curr;
+        operations += 3;
+
+        printf("%d ", curr);
+        operations++; 
+    }
+}
+
+int main() {
+    int n = 9;
+    operations++; 
+
+    printFib(n);
+
+    printf("\nTotal operations performed: %d\n", operations);
+    operations++; 
+
+    return 0;
+}
