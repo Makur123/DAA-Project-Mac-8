43fc5c6127149e080785c4541673df75c4474ec4
Makur123 <makurdhuor2019@gmail.com>
2025-03-26 04:54:08 +0530

    Edited the recrusive code

--- fibonacci_recusive.c
+++ fibonacci_recusive.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
-
-int fibonacci(int n){
-    if(n<=0)
-        return 0;
-    else if(n==1)
-        return 1;
-    else
-        return fibonacci(n-1)+fibonacci(n-2);
-}
-
-int main(){
-    int n =5;
-    int result = fibonacci(n);
-    printf("%d\n", result);
-    return 0;
-}
+static int count = 0;
+int fibbonacci(int n) {
+    if(n == 0){
+       return 0;
+       count++;
+    } else if(n == 1) {
+       return 1;
+       count++;
+    } else {
+       return (fibbonacci(n-1) + fibbonacci(n-2));
+       count++;
+    }
+ }
+ 
+ int main() {
+    int n = 5;
+    int i;
+    printf("Fibbonacci of %d: " , n);
+     
+    for(i = 0;i<n;i++) {
+       printf("%d ",fibbonacci(i)); 
+       count++;           
+    }
+    printf("\nTime complexity : %d\n", count);
+ }
